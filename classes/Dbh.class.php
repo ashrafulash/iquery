2a9606a612642c3705c5cd061113c5855b2db064
Md. Ashraful Mollah <ashrafulash@github.com>
2020-05-09 16:17:06 +0600

    db connect

---
+++ classes/Dbh.class.php
@@ -0,0 +1,20 @@
+<?php
+class Dbh{
+    // private $host   = 'localhost';
+    // private $user   = 'themxygf_quiz';
+    // private $pwd    = 'tr{]zO&@MQ~S';
+    // private $dbName = 'themxygf_iquery';
+
+
+    private $host   = 'localhost';
+    private $user   = 'root';
+    private $pwd    = '';
+    private $dbName = 'iquery'; 
+
+    protected function connect(){
+        $dsn    = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
+        $pdo    = new PDO($dsn, $this->user, $this->pwd);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+    }
+}
